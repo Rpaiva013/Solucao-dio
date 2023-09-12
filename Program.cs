59cebd26a31e773e79a292f01f68dc612dfa5a09
Renan Paiva <renan.paiva@ltm.digital>
2023-09-12 14:42:37 -0300

    terceiro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
-
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia (numero: "1234", modelo: "Modelo 1", imei: "324531", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone phone = new Iphone (numero: "1234", modelo: "Modelo 1", imei: "324531", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
